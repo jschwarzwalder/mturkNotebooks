92f24d5a4f756a53e45230ea597e466e7a2bd9e9
Jami Schwarzwalder <jamschwa@amazon.com>
2017-10-11 14:27:50 -0700

    adding description

--- final/image-categorization-test.py
+++ final/image-categorization-test.py
@@ -26,21 +26,25 @@ image_url = 'https://requester.mturk.com/assets/simon.jpg'
 
 # The type of thing we're looking for
 label1 = 'Plant'
+description1 =  'multicellular photosynthetic eukaryotes often refered to as green plants.'
 label2 = 'Animal'
+description2 =  'multicellular biological eukaryotes which includes vertebrates like fish, reptitles, birds and mammels, and invertebrates like worms and fruit flies.'
 label3 = 'Bacteria'
+description3 =  'small prokaryotic microorganisms about a few micrometers in length.'
 label4 = 'Fungi'
+description4 =  'eukayotic organisms such as yeasts and molds.'
 label5 = 'Protists'
+description5 =  'unicellular eukayotic organisms that often cluster together.'
 
 # Create the task
 put_result = crowd_client.put_task(function_name,
                                    task_name,
                                    {'image': {'url': image_url},
-                                   'categories': [ {'label': label1},
-                                                   {'label': label2},
-                                                   {'label': label3},
-                                                   {'label': label4},
-                                                   {'label': label5}] })
-
+                                   'categories': [ {'label': label1,  "description":  description1 },
+                                                   {'label': label2,  "description":  description2 },
+                                                   {'label': label3,  "description":  description3 },
+                                                   {'label': label4,  "description":  description4 },
+                                                   {'label': label5,  "description":  description5 }] })
 
 print('PUT response: {}'.format(
     {'status_code': put_result.status_code, 'task': put_result.json()}))
