2928013f617d0b6e1315198edca475b43870d70f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-12 18:45:03 -0700

    code for getting results

--- image-categorization-result.py
+++ image-categorization-result.py
@@ -15,13 +15,42 @@ crowd_client = MTurkCrowdClient(session)
 task_name = 'my-test-task-2c28dbb029f4400a8d90219b3909d7da'
 
 # Next, we specify the name of the function to call
-function_name = 'image-categorization'
+function_name = 'image-similarity'
 
 
+task_name = 'my-test-task-27f7a6f1052540dd8e4a29298f5d8967'
 
 # Get the task we just created. Note that for a prod (i.e., non-test) task,
 # we'd have to poll periodically until the task completed.
 get_result = crowd_client.get_task(function_name, task_name)
 
 print('GET response: {}'.format(
-{'status_code': get_result.status_code, 'task': get_result.json()}))
+{'status_code': get_result.status_code, 'name': function_name, 'task': get_result.json()}))
+
+
+
+function_name = 'text-intent-detection'
+
+
+task_name = 'my-test-task-326f0ffff29b40d8847f4545fd570d5d'
+
+# Get the task we just created. Note that for a prod (i.e., non-test) task,
+# we'd have to poll periodically until the task completed.
+get_result = crowd_client.get_task(function_name, task_name)
+
+print('GET response: {}'.format(
+{'status_code': get_result.status_code, 'name': function_name, 'task': get_result.json()}))
+
+
+
+function_name = 'text-categorization'
+
+
+task_name = 'my-test-task-58a07ec626ee492b93a679e5cad79a2d'
+
+# Get the task we just created. Note that for a prod (i.e., non-test) task,
+# we'd have to poll periodically until the task completed.
+get_result = crowd_client.get_task(function_name, task_name)
+
+print('GET response: {}'.format(
+{'status_code': get_result.status_code, 'name': function_name, 'task': get_result.json()}))
