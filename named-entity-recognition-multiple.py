728f13a86529445d779101941eac8b23d2498157
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-13 16:29:43 -0700

    Named Entity Recognition, calling multiples

---
+++ named-entity-recognition-multiple.py
@@ -0,0 +1,37 @@
+from mturk_crowd_beta_client import MTurkCrowdClient
+from boto3.session import Session
+import csv
+
+# This examples assume you have a local AWS profile called
+# 'mturk-crowd-caller', but you can authenticate however you like,
+# including by directly passing in your access key and secret key.
+session = Session(profile_name='mturk-crowd-caller')
+
+# Create the client
+crowd_client = MTurkCrowdClient(session)
+
+# Next, we specify the name of the function to call
+function_name = 'named-entity-recognition'
+
+with open('NER.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+        task_name = row[0]
+        text = row[1]
+
+
+        put_result = crowd_client.put_task(function_name,
+                                            task_name,
+                                            {'text': text,})
+
+
+        print('PUT response: {}'.format(
+            {'status_code': put_result.status_code, 'task': put_result.json()}))
+
+
+
+        get_result = crowd_client.get_task(function_name, task_name)
+
+        print('GET response: {}'.format(
+            {'status_code': get_result.status_code, 'task': get_result.json()}))
