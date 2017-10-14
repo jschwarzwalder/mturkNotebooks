b0229fe76a9467fe5919b6819773b6c336b3c1a2
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-14 15:41:01 -0700

    Making example for key phrase extration

---
+++ final/key-phrase-extraction.py
@@ -0,0 +1,40 @@
+from mturk_crowd_beta_client import MTurkCrowdClient
+from boto3.session import Session
+import uuid
+
+# This examples assume you have a local AWS profile called
+# 'mturk-crowd-caller', but you can authenticate however you like,
+# including by directly passing in your access key and secret key.
+session = Session(profile_name='mturk-crowd-caller')
+
+# Create the client
+crowd_client = MTurkCrowdClient(session)
+
+# For this example, we'll give our task a random, unique name. For prod
+# tasks, you'll probably want to pick a name based on your input source.
+task_name = 'my-test-task-' + uuid.uuid4().hex
+
+# Next, we specify the name of the function to call
+function_name = 'key-phrase-extraction' 
+
+# We ask the worker to label the entities in some some text
+# (companies, events, locations, organizations, people, quantities)
+text = 'Android/Windows guy... Your statement completely undermines the concept of "user experience." Apple advanced UX by decades.'
+
+
+# Create the task
+put_result = crowd_client.put_task(function_name,
+                                   task_name,
+                                   {'text': text,})
+
+
+print('PUT response: {}'.format(
+    {'status_code': put_result.status_code, 'task': put_result.json()}))
+
+
+# Get the task we just created. Note that for a prod (i.e., non-test) task,
+# we'd have to poll periodically until the task completed.
+get_result = crowd_client.get_task(function_name, task_name)
+
+print('GET response: {}'.format(
+{'status_code': get_result.status_code, 'task': get_result.json()}))
