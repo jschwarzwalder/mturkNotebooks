9965e52e435d2f0cdcd8b68cb90e0082edda4360
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-18 23:36:06 -0700

    calling image similarity multiple times

--- image-similarity-multiple.py
+++ image-similarity-multiple.py
@@ -1,6 +1,6 @@
 from mturk_crowd_beta_client import MTurkCrowdClient
 from boto3.session import Session
-import uuid
+import csv
 
 # This examples assume you have a local AWS profile called
 # 'mturk-crowd-caller', but you can authenticate however you like,
@@ -10,30 +10,29 @@ session = Session(profile_name='mturk-crowd-caller')
 # Create the client
 crowd_client = MTurkCrowdClient(session)
 
-# For this example, we'll give our task a random, unique name. For prod
-# tasks, you'll probably want to pick a name based on your input source.
-task_name = 'my-test-task-' + uuid.uuid4().hex
-
 # Next, we specify the name of the function to call
 function_name = 'image-similarity'
 
-# The text we want to compare
-image1 = 'https://requester.mturk.com/assets/lucy.jpg'
-image2 = 'https://requester.mturk.com/assets/gold-finding-puppy.jpg'
-
-# Create the task
-put_result = crowd_client.put_task(function_name,
-                                   task_name,
-                                   {'image1': {'url': image1}, 'image2': {'url': image2}})
-
-print('PUT response: {}'.format(
-    {'status_code': put_result.status_code, 'task': put_result.json()}))
+with open('image-comparison-inputs.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+        task_name = row[0]
+        image1 = row[1]
+        image2 = row[2]
 
 
+        put_result = crowd_client.put_task(function_name,
+                                            task_name,
+                                            {'image1': {'url': image1}, 'image2': {'url': image2}})
 
-# Get the task we just created. Note that for a prod (i.e., non-test) task,
-# we'd have to poll periodically until the task completed.
-get_result = crowd_client.get_task(function_name, task_name)
 
-print('GET response: {}'.format(
-    {'status_code': get_result.status_code, 'task': get_result.json()}))
+        print('PUT response: {}'.format(
+            {'status_code': put_result.status_code, 'task': put_result.json()}))
+
+
+
+        get_result = crowd_client.get_task(function_name, task_name)
+
+        print('GET response: {}'.format(
+            {'status_code': get_result.status_code, 'task': get_result.json()}))
