dd38db36d42684a3a5e59767f3ab046073a51aa7
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-25 15:14:38 -0700

    updating input

--- final/key-phrase-extraction.py
+++ final/key-phrase-extraction.py
@@ -19,8 +19,7 @@ function_name = 'key-phrase-extraction'
 
 # We ask the worker to label the entities in some some text
 # (companies, events, locations, organizations, people, quantities)
-text = 'Android/Windows guy... Your statement completely undermines the concept of "user experience." Apple advanced UX by decades.'
-
+text = 'I love this album. I especially like Peter\'s version of "The Parting Glass" and his collaboration with Jackie Evancho on "Hallelujah".'
 
 # Create the task
 put_result = crowd_client.put_task(function_name,
