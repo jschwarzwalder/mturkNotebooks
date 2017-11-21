7d5246ebfe8d77ac7c87a0f7ac14ab35fc7028ba
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-11-21 13:02:55 -0800

    updating to open source image

--- final/bounding-box-test.py
+++ final/bounding-box-test.py
@@ -12,30 +12,31 @@ crowd_client = MTurkCrowdClient(session)
 
 # For this example, we'll give our task a random, unique name. For prod
 # tasks, you'll probably want to pick a name based on your input source.
-task_name = 'my-test-task-' + uuid.uuid4().hex
+task_name = 'my-test-task-a8b01918e29a4cbc948abab7e4f9b718'
 
 # Next, we specify the name of the function to call
 # In this example, we're first calling the bounding-box-test function.
 # The test function doesn't cost any money and is useful for validating that your account is setup correctly and for testing your integration.
 # To call the prod bounding-box function, uncomment the next line and comment out the bounding-box-test line
 # function_name = 'bounding-box'
-function_name = 'bounding-box-test'
+function_name = 'bounding-box'
 
-# The image we want labeled
-image_url = 'https://sierra-videos.s3.amazonaws.com/2017_0521/DJI_0001_frames_720h/1495411174030000000.jpg'
-
-# The type of thing we're ask workers to find in image
-# You can ask workers to label multiple types of things if needed.
-label = 'car'
-
-# Create the task
-put_result = crowd_client.put_task(function_name,
-                                   task_name,
-                                   {'image': {'url': image_url}, "labels": [{"label": label}]})
-
-print('PUT response: {}'.format(
-    {'status_code': put_result.status_code, 'task': put_result.json()}))
-
+# # The image we want labeled
+# image_url = 'https://s3-us-west-2.amazonaws.com/mturk-sample-media/box-hit-example.jpg'
+#
+# # The type of thing we're ask workers to find in image
+# # You can ask workers to label multiple types of things if needed.
+# label = 'pedestrian'
+#
+# #Create the task
+# put_result = crowd_client.put_task(function_name,
+#                                    task_name,
+#                                    {'image': {'url': image_url}, "labels": [{"label": label}]})
+#
+#
+# print('PUT response: {}'.format(
+#     {'status_code': put_result.status_code, 'task': put_result.json()}))
+#
 
 # Get the task we just created. Note that for a prod (i.e., non-test) task,
 # we'd have to poll periodically until the task completed.
